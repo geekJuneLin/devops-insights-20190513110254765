9e1e1e0e5a3010f3923bbcb83a315c538bd92861
Junyu Lin <563182850@qq.com>
2019-05-15 02:59:18 +0000

    fix bugs

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -97,7 +97,7 @@
         main: {
           temp: 78
         },
-        coor:{lat: -30.442,lon: 170.323}
+        coor:{la: -30.442,lo: 170.323}
       };
 
       var request = function( obj, callback ){
